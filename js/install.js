a7bfff5bac61b50a92a85450c906febe2d16ebc9
Max <max30012540@hotmail.com>
2020-09-04 21:22:24 +0700

    Add install to PC button

--- js/install.js
+++ js/install.js
@@ -3,21 +3,25 @@ let deferredInstallPrompt = null;
 window.addEventListener('beforeinstallprompt', (evt) => {
     console.log("beforeinstallprompt evant", evt);
     deferredInstallPrompt = evt;
+
+    $("#install-pwa").show();
 });
 
-let installPWA = () => {
+$("#install-pwa").click(() => {
     deferredInstallPrompt.prompt();
 
     deferredInstallPrompt.userChoice.then((choice) => {
         if (choice.outcome === 'accepted') {
             console.log('User accepted the A2HS prompt', choice);
+            $("#install-pwa").hide();
         } else {
             console.log('User dismissed the A2HS prompt', choice);
         }
         deferredInstallPrompt = null;
     });
-};
+});
 
 window.addEventListener('appinstalled', () => {
     console.log('App installed.', evt);
+    $("#install-pwa").hide();
 });
