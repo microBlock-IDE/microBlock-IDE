0993cb7d52048fc6692c41e6734a424ca594e023
Max <max30012540@hotmail.com>
2020-09-06 02:28:58 +0700

    Add sign in with GitHub

--- js/GitHubAPI.js
+++ js/GitHubAPI.js
@@ -55,3 +55,62 @@ let downloadFile = async (url, saveAs) => {
 
     return true;
 }
+
+// GitHub
+const github_client_id = "5fd432a542e37673decb";
+
+$("#github-signin").click(() => {
+    window.location.href = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${github_client_id}&redirect_uri=${window.location.href.replace(/\?.*/, "")}`;
+});
+
+(async () => {
+    // Chack 'code' parameter
+    await (async () => {
+        let m = /code=([^&]+)/.exec(window.location.href);
+        if (m) {
+            let code = m[1];
+            console.log("GitHub Code:", code);
+            
+            let access_token = await fetch("https://us-central1-ublock-c0a08.cloudfunctions.net/github/get_token", { 
+                method: "post",
+                body: code,
+                redirect: "follow"
+            });
+
+            if (access_token.status !== 200) {
+                console.error("GitHub Get token error", await access_token.text());
+                return;
+            }
+
+            access_token = await access_token.text();
+            localStorage.setItem("access_token", access_token);
+        }
+    })();
+
+    // Get user profile
+    await (async () => {
+        let github_token = localStorage.getItem("access_token");
+        if (github_token) {
+            let user_info = await fetch("https://api.github.com/user", { 
+                redirect: "follow",
+                headers: { 
+                    "Authorization": `token ${github_token}`,
+                    "Accept": "application/json"
+                }
+            });
+
+            if (user_info.status !== 200) {
+                console.error("GitHub Get User Info error", await access_token.text());
+                return;
+            }
+
+            user_info = await user_info.json();
+            console.log("User Info", user_info);
+
+            $("#github_avatar").attr("src", user_info.avatar_url).show();
+            $("#github-signin").hide();
+
+            NotifyS(`Sign in with GitHub (Username: ${user_info.login})`)
+        }
+    })();
+})();
