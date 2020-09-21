8f1954c4af3d6df0964301f5cec55d684a0cc3eb
Max <max30012540@hotmail.com>
2020-09-21 20:03:50 +0700

    Future Electron: Fix bug script order

--- boards/index.js
+++ boards/index.js
@@ -1,7 +1,8 @@
 let loadScript = (f) => {
-    let script = document.createElement("script");
+    /*let script = document.createElement("script");
     script.setAttribute("src", f);
-    document.body.appendChild(script);
+    document.body.appendChild(script);*/
+    document.write(`<script src="${f}"></script>`);
 };
 
 loadScript(`${rootPath}/boards/kidbright32-v1.3/index.js`); // Add KidBright32 V1.3 & V1.4
