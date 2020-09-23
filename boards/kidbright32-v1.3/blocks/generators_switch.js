9a5f8f992260e29876e8de843080ba6f54e37194
Max <sonthaya.nt@hotmail.com>
2020-09-23 20:48:53 +0700

    Fix, global all variables

--- boards/kidbright32-v1.3/blocks/generators_switch.js
+++ boards/kidbright32-v1.3/blocks/generators_switch.js
@@ -31,9 +31,33 @@ Blockly.Python['switch_on_press'] = function(block) {
     var dropdown_pin = block.getFieldValue('pin');
     var statements_callback = Blockly.Python.statementToCode(block, 'callback');
 
+    // -----------------------------
+    var globals = [];
+    var varName;
+    var workspace = block.workspace;
+    var variables = Blockly.Variables.allUsedVarModels(workspace) || [];
+    for (var i = 0, variable; variable = variables[i]; i++) {
+      varName = variable.name;
+      if (block.getVars().indexOf(varName) == -1) {
+        globals.push(Blockly.Python.variableDB_.getName(varName,
+            Blockly.VARIABLE_CATEGORY_NAME));
+      }
+    }
+    // Add developer variables.
+    var devVarList = Blockly.Variables.allDeveloperVariables(workspace);
+    for (var i = 0; i < devVarList.length; i++) {
+      globals.push(Blockly.Python.variableDB_.getName(devVarList[i],
+          Blockly.Names.DEVELOPER_VARIABLE_TYPE));
+    }
+  
+    globals = globals.length ?
+        Blockly.Python.INDENT + 'global ' + globals.join(', ') + '\n' : '';
+    // -----------------------------
+
     var functionName = Blockly.Python.provideFunction_(
         dropdown_pin + 'PressCB',
         ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '():',
+        globals,
         statements_callback]);
 
     var code = `switch.press(switch.${dropdown_pin}, ${functionName})\n`;
@@ -46,9 +70,33 @@ Blockly.Python['switch_on_release'] = function(block) {
     var dropdown_pin = block.getFieldValue('pin');
     var statements_callback = Blockly.Python.statementToCode(block, 'callback');
 
+    // -----------------------------
+    var globals = [];
+    var varName;
+    var workspace = block.workspace;
+    var variables = Blockly.Variables.allUsedVarModels(workspace) || [];
+    for (var i = 0, variable; variable = variables[i]; i++) {
+      varName = variable.name;
+      if (block.getVars().indexOf(varName) == -1) {
+        globals.push(Blockly.Python.variableDB_.getName(varName,
+            Blockly.VARIABLE_CATEGORY_NAME));
+      }
+    }
+    // Add developer variables.
+    var devVarList = Blockly.Variables.allDeveloperVariables(workspace);
+    for (var i = 0; i < devVarList.length; i++) {
+      globals.push(Blockly.Python.variableDB_.getName(devVarList[i],
+          Blockly.Names.DEVELOPER_VARIABLE_TYPE));
+    }
+  
+    globals = globals.length ?
+        Blockly.Python.INDENT + 'global ' + globals.join(', ') + '\n' : '';
+    // -----------------------------
+
     var functionName = Blockly.Python.provideFunction_(
         dropdown_pin + 'ReleaseCB',
         ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '():',
+        globals,
         statements_callback]);
 
     var code = `switch.release(switch.${dropdown_pin}, ${functionName})\n`;
