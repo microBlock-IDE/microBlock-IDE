3121fc58dec232764fdad7450453de4e49016465
Max <sonthaya.nt@hotmail.com>
2020-11-29 22:15:34 +0700

    Move new windows to Main process

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,19 +1,3 @@
 $("#open-dashboard").click(() => {
-    if (sharedObj.dashboardWin && !sharedObj.dashboardWin.isDestroyed()) {
-        sharedObj.dashboardWin.focus();
-        return;
-    }
-
-    sharedObj.dashboardWin = new remote.BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        },
-        icon: path.join(`${rootPath}/favicon.png`)
-    });
-
-    sharedObj.dashboardWin.loadFile(`${rootPath}/dashboard/index.html`);
-    
-    // sharedObj.dashboardWin.maximize();
+    ipcRenderer.send("show-dashboard");
 });
