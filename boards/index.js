14e49d6074a9b48e9818934e5e26e6a9b7b7c14a
Max <sonthaya.nt@hotmail.com>
2021-01-29 02:53:23 +0700

    Move KidBright32 V1.5 Rev3.1 to last item

--- boards/index.js
+++ boards/index.js
@@ -7,9 +7,9 @@ let loadScript = (f) => {
 
 loadScript(`${rootPath}/boards/kidbright32/index.js`); // Add KidBright32 common config
 loadScript(`${rootPath}/boards/kidbright32-v1.3/index.js`);  // Add KidBright32 V1.3 & V1.4
-loadScript(`${rootPath}/boards/kidbright32-v1.5/index.js`);  // Add KidBright32 V1.5
 loadScript(`${rootPath}/boards/kidbright32i/index.js`); // Add KidBright32i by INEX
 loadScript(`${rootPath}/boards/kidbright32-v1.6/index.js`);  // Add KidBright32 V1.6 by Gravitech
 loadScript(`${rootPath}/boards/openkb/index.js`);  // Add OpenKB
 loadScript(`${rootPath}/boards/ipst-wifi/index.js`);  // Add IPST-WiFi
+loadScript(`${rootPath}/boards/kidbright32-v1.5/index.js`);  // Add KidBright32 V1.5
 // loadScript(`${rootPath}/boards/ttgo-t-display/index.js`);  // Add TTGO T-Display
