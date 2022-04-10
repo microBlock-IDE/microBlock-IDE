340d177dd7f1995f1fa649304810276660fb4f4d
Max <sonthaya.nt@hotmail.com>
2022-04-11 03:59:48 +0700

    Add code example work

--- js/code-example.js
+++ js/code-example.js
@@ -1,6 +1,3 @@
-
-setTimeout(() => ShowDialog($("#code-example-dialog")), 100);
-
 $("#code-example-mode-select-switch > li").click(async function () {
     let value = $(this).attr("data-value");
     /*if (value == 1) { // Block mode
@@ -85,3 +82,80 @@ $("#code-example-mode-select-switch > li").click(async function () {
     $("#code-example-mode-select-switch > li").removeClass("active");
     $(this).addClass("active");
 });
+
+const blocklyDivExampleCode = document.querySelector("#blocklyDivExampleCode");
+
+blocklyWorkspaceExampleCode = Blockly.inject(blocklyDivExampleCode, {
+    media: 'blockly/media/',
+    toolbox: "",
+    grid : {
+		spacing : 25, 
+		length : 1, 
+		colour : '#888', 
+		snap : true
+    },
+    trashcan : true,
+    zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 1,
+        maxScale: Infinity,
+        minScale: 0.3,
+        scaleSpeed: 1.2
+    },
+    scrollbars : true,
+    comments : true, 
+	disable : true, 
+    maxBlocks : Infinity, 
+    rtl : false, 
+    oneBasedIndex : false, 
+    sounds : true, 
+    readOnly: true
+});
+
+const openExampleDialog = () => {
+    $("#example-list-item").html("");
+    
+    // Get board example
+    const board = boards.find(board => board.id === boardId);
+    
+    $("#example-list-item").append(`<li class="sub-header">Board Example</li>`);
+    (board?.examples || []).forEach((item, index) => {
+        $("#example-list-item").append(`
+            <li>
+                <a 
+                    href="#" 
+                    data-index="${index}" 
+                    data-type="board"
+                    data-files="${item?.files || ""}"
+                >${item.name}</a></li>
+        `);
+    });
+
+    $("#example-list-item > li > a").click(async function(e) {
+        e.preventDefault();
+
+        const rawFileData = await (await fetch(`boards/${boardId}/${$(this).attr("data-files")}.mby`)).text();
+        const local_vFSTree = JSON.parse(rawFileData);
+        const xmlCode = local_vFSTree["main.xml"];
+
+        blocklyWorkspaceExampleCode.clear();
+        try {
+            Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xmlCode), blocklyWorkspaceExampleCode);
+        } catch (e) {
+            console.log(e);
+        }
+        blocklyWorkspaceExampleCode.scrollCenter();
+    
+        $("#example-list-item > li").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+    
+    // console.log(board?.examples || []);
+    ShowDialog($("#code-example-dialog"));
+    Blockly.svgResize(blocklyWorkspaceExampleCode);
+}
+
+setTimeout(() => {
+    openExampleDialog();
+}, 1000);
