a0f12cf2cd65ee243f2205277335d6436077d2fc
Max <7087907+maxpromer@users.noreply.github.com>
2023-01-29 00:07:23 +0700

    Add move spin block

---
+++ boards/puppy-bot/blocks/blocks_motor.js
@@ -0,0 +1,154 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "motor_forward",
+        "message0": "move forward at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed"
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_backward",
+        "message0": "move backward at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed",
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_turn_left",
+        "message0": "turn left at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed",
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_turn_right",
+        "message0": "turn right at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed",
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_spin_left",
+        "message0": "spin left at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed",
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_spin_right",
+        "message0": "spin right at power %1 %% for %2 secs",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed",
+        }, {
+            "type": "input_value",
+            "name": "time",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_move",
+        "message0": "%1 at power %2 %%",
+        "args0": [{
+            "type": "field_dropdown",
+            "name": "move",
+            "options": [
+                ["move forward", "1"],
+                ["move backward", "2"],
+                ["turn left", "3"],
+                ["turn right", "4"]
+            ]
+        }, {
+            "type": "input_value",
+            "name": "speed",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_wheel",
+        "message0": "left wheel turns at power %1 %%, right wheel turns at power %2 %%",
+        "args0": [{
+            "type": "input_value",
+            "name": "speed1",
+        }, {
+            "type": "input_value",
+            "name": "speed2",
+        }],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "motor_stop",
+        "message0": "stop moving",
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": "#28B463",
+        "tooltip": "",
+        "helpUrl": ""
+    }
+]);
